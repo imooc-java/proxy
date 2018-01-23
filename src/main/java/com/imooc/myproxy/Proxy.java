0c21d3dbd8fccd1260140446fe54e7e9df98a301
CorningSun <corningsun@163.com>
2018-01-23 17:14:41 +0800

    3-2 完善动态代理实现

--- src/main/java/com/imooc/myproxy/Proxy.java
+++ src/main/java/com/imooc/myproxy/Proxy.java
@@ -1,13 +1,21 @@
 package com.imooc.myproxy;
 
+import com.imooc.proxy.Car;
 import org.apache.commons.io.FileUtils;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.Charset;
 
 /**
  * Created by corning on 2018/1/23.
@@ -16,21 +24,37 @@ public class Proxy {
 
     public static final String CHARSET = "UTF-8";
 
-    public static Object newProxyInstance(Class infce) throws IOException {
+    public static Object newProxyInstance(Class infce) throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        // 生产代理类内容
         String proxyClassContent = getProxyClassContent(infce);
+        // 获取代理类 java 文件名
+        String proxyJavaFileName = getProxyJavaFileName();
+        // 将代理类写到 java 文件中
+        writeAsJava(proxyJavaFileName, proxyClassContent);
+        // 对代理类进行编译(javac xxx.java)
+        compilerProxyFile(proxyJavaFileName);
+        // 加载编译好的 class 文件
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        Class<?> proxyClass = classLoader.loadClass("com.imooc.myproxy.$Proxy0");
+        Constructor<?> constructor = proxyClass.getConstructor(infce);
+        return constructor.newInstance(new Car());
+    }
 
-        writeAsJava(proxyClassContent);
-
-        return null;
+    private static void compilerProxyFile(String proxyJavaFileName) throws IOException {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, Charset.forName(CHARSET));
+        Iterable<? extends JavaFileObject> units = fileManager.getJavaFileObjects(proxyJavaFileName);
+        JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, null, null, null, units);
+        task.call();
+        fileManager.close();
     }
 
     /**
      * 通过代理模版 proxyClass.vm 和 接口类 自动生成代理类
      *
-     * @see Velocity
-     *
      * @param infce
      * @return
+     * @see Velocity
      */
     static String getProxyClassContent(Class infce) {
         Velocity.init();
@@ -48,10 +72,13 @@ public class Proxy {
         return proxyWriter.toString();
     }
 
-    static void writeAsJava(String classStr) throws IOException {
+    static void writeAsJava(String proxyJavaFileName, String classStr) throws IOException {
+        FileUtils.writeStringToFile(new File(proxyJavaFileName), classStr, CHARSET);
+    }
+
+    private static String getProxyJavaFileName() {
         String path = Proxy.class.getResource(".").getFile();
-        String filePath = path + "$Proxy.java";
-        FileUtils.writeStringToFile(new File(filePath), classStr, CHARSET);
+        return path + "$Proxy0.java";
     }
 
 }
--- src/test/java/com/imooc/myproxy/ProxyTest.java
+++ src/test/java/com/imooc/myproxy/ProxyTest.java
@@ -3,20 +3,14 @@ package com.imooc.myproxy;
 import com.imooc.proxy.Moveable;
 import org.junit.Test;
 
-import java.io.IOException;
-
 /**
  * Created by corning on 2018/1/23.
  */
 public class ProxyTest {
     @Test
     public void newProxyInstance() throws Exception {
-        System.out.println(Proxy.newProxyInstance(Moveable.class));
-    }
-
-    @Test
-    public void write2Java() throws IOException {
-        Proxy.writeAsJava("xxx");
+        Moveable m = (Moveable) Proxy.newProxyInstance(Moveable.class);
+        m.move();
     }
 
     @Test
